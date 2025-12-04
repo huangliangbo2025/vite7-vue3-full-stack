59ad615db7b58c8cf923121ed501973319836c04
xiaobu <864181495@qq.com>
2025-12-04 16:24:27 +0800

    feat: 需改unocss，用于支持text-primary/bg-primary

--- uno.config.ts
+++ uno.config.ts
@@ -10,4 +10,64 @@ export default defineConfig({
   transformers: [
     transformerVariantGroup(), // 支持 hover:(bg-red text-white)
   ],
+
+  // safelist: 'prose prose-sm m-auto text-left'.split(' '),
+  // 预设主演颜色的指令
+  theme: {
+    colors: {
+      // 定义主题颜色变量（实际通过CSS变量控制）
+      'brand': {
+        primary: 'hsl(var(--hue, 217) 78% 51%)', // class="bg-brand-primary"
+        DEFAULT: '#942192', // class="bg-brand"
+      },
+    },
+  },
+
+  rules: [
+    // START  ============ element主题规则预设 text-primary-light-3 => color: var(--el-color-primary-light-3)
+    [
+      /^text-(primary|success|warning|danger|error|info)/,
+      ([, type]) => ({
+        color: `var(--el-color-${type})`,
+      }),
+    ],
+    [
+      /^bg-(primary|success|warning|danger|error|info)/,
+      ([, type]) => ({
+        'background-color': `var(--el-color-${type})`,
+      }),
+    ],
+    [
+      /^text-(primary|success|warning|danger|error|info)-light-(\d+)$/,
+      ([, type, num]) => ({
+        color: `var(--el-color-${type}-light-${num})`,
+      }),
+    ],
+    [
+      /^bg-(primary|success|warning|danger|error|info)-light-(\d+)$/,
+      ([, type, num]) => ({
+        'background-color': `var(--el-color-${type}-light-${num})`,
+      }),
+    ],
+    [
+      /^text-(primary|success|warning|danger|error|info-dark)/,
+      ([, type]) => ({
+        color: `var(--el-color-${type}-dark-2)`,
+      }),
+    ],
+    // END ==========================================
+    // 多文本溢出出现省略号 text-ellipsis-1 => '-webkit-line-clamp': 1,
+    [
+      /^text-ellipsis-(\d+)$/,
+      ([, lineNum]) => ({
+        'display': '-webkit-box',
+        '-webkit-line-clamp': lineNum, // 提取行数参数
+        '-webkit-box-orient': 'vertical',
+        'overflow': 'hidden',
+        'text-overflow': 'ellipsis',
+      }),
+      // 规则优先级（可选，确保覆盖默认样式）
+      { layer: 'utilities', priority: 10 },
+    ],
+  ],
 })
