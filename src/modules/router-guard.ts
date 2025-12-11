f9391b76e09a05b97975d0452beb2d8fc9e94993
xiaobu <864181495@qq.com>
2025-12-11 22:42:04 +0800

    feat: 添加路由权限

--- src/modules/router-guard.ts
+++ src/modules/router-guard.ts
@@ -1,11 +1,29 @@
+import { useAuthStore } from '@/stores'
 import type { UserModule } from '@/types/modules'
 import NProgress from 'nprogress'
+
+const WHITE_LIST = ['/auth/login']
 
 export const install: UserModule = ({ app, router }) => {
   app.use(router)
 
-  router.beforeEach(async (_to, _from, next) => {
+  router.beforeEach(async (to, _from, next) => {
     NProgress.start()
+    if (WHITE_LIST.includes(to.path)) {
+      next()
+      return
+    }
+
+    const { token } = useAuthStore()
+
+    if (!token) {
+      next({
+        path: '/auth/login',
+        replace: true,
+      })
+      return
+    }
+
     next()
   })
 
