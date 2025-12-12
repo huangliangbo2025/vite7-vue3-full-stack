c7f86b764fb3570b636934fd05dfbff4c40d6896
xiaobu <864181495@qq.com>
2025-12-12 10:23:49 +0800

    feat: 添加退出登录功能

--- src/layouts/components/Header.vue
+++ src/layouts/components/Header.vue
@@ -1,17 +1,20 @@
 <script setup lang="ts">
 import { Expand } from '@element-plus/icons-vue'
+import UserInfo from './UserInfo.vue'
 
 const circleUrl = 'https://cube.elemecdn.com/3/7c/3ea6beec64369c2642b92c6726f1epng.png'
 </script>
 
 <template>
-  <div class="flex h-full items-center justify-between pr-3">
-    <el-button size="small" type="text">
+  <div class="flex h-full items-center justify-between px-5">
+    <el-button size="small" type="text" class="!p-0">
       <el-icon class="text-info-light-3 text-5 cursor-pointer">
         <Expand />
       </el-icon>
     </el-button>
-    <el-avatar shape="circle" :size="36" :src="circleUrl" />
+    <div>
+      <UserInfo />
+    </div>
   </div>
 </template>
 
---
+++ src/layouts/components/UserInfo.vue
@@ -0,0 +1,64 @@
+<script setup lang="ts">
+import { storeToRefs } from 'pinia'
+import { useTemplateRef } from 'vue'
+import { useAuthStore } from '@/stores'
+
+const router = useRouter()
+
+const avatar = 'https://cube.elemecdn.com/9/c2/f0ee8a3c7c9638a54940382568c9dpng.png'
+
+const { clearUserInfo } = useAuthStore()
+const { token, userInfo } = storeToRefs(useAuthStore())
+const isLogin = computed(() => !!token.value)
+const popoverRef = useTemplateRef('popoverRef')
+const onClickOutside = () => {
+  popoverRef.value!.hide()
+}
+
+const loginOut = () => {
+  clearUserInfo()
+  router.push('/auth/login')
+}
+</script>
+
+<template>
+  <el-space v-if="!isLogin">
+    <el-button outline-blue> 注册 </el-button>
+    <el-button type="primary" @click="() => router.push('/login')"> 登录 </el-button>
+  </el-space>
+
+  <template v-else>
+    <div v-popover="popoverRef" class="flex-center gap-3px flex cursor-pointer items-center">
+      <el-avatar class="h-32px w-32px" :src="avatar" />
+    </div>
+
+    <el-popover
+      ref="popoverRef"
+      trigger="click"
+      virtual-triggering
+      persistent
+      width="240"
+      v-click-outside="onClickOutside"
+    >
+      <el-row align="middle">
+        <el-col :span="24">
+          <div class="flex items-center gap-3">
+            <el-avatar class="h-36px w-36px" :src="avatar" />
+            <div class="text-12px color-[var(--el-text-color-regular)] leading-18px">
+              <p>
+                {{ userInfo?.username }}
+              </p>
+              <p>超级管理员</p>
+            </div>
+          </div>
+        </el-col>
+        <el-col :span="24">
+          <el-divider class="my-3" />
+        </el-col>
+        <el-col :span="24">
+          <el-button type="danger" class="w-full" @click="loginOut"> 退出登录 </el-button>
+        </el-col>
+      </el-row>
+    </el-popover>
+  </template>
+</template>
